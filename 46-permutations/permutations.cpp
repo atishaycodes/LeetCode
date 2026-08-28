e8b2757aa29990169a204ba2d7e5ae3bd4bdcc59
Atishay <atishay.tech57@gmail.com>
2026-08-28 12:18:37 +0530

    Time: 11 ms (5.68%) | Memory: 15.3 MB (5.03%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,16 +1,17 @@
 class Solution {
     private:
-    void solve(vector<int>& nums, vector<vector<int>> &res, int &n, vector<int> temp){
+    void solve(vector<int>& nums, vector<vector<int>> &res, int &n, vector<int> temp, unordered_set<int> st){
         if(temp.size()==n){
             res.push_back(temp);
             return;
         }
         for(int i=0; i<n; i++){
-            if(find(temp.begin(), temp.end(), nums[i])==temp.end()){
+            if(st.find(nums[i])==st.end()){
                 temp.push_back(nums[i]);
-                solve(nums, res, n, temp);
+                st.insert(nums[i]);
+                solve(nums, res, n, temp, st);
                 temp.pop_back();
-               
+                st.erase(nums[i]);
             }
         }
     }
@@ -18,8 +19,9 @@ public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> res;
         vector<int> temp;
+        unordered_set<int> st;
         int n= nums.size();
-        solve(nums, res, n, temp);
+        solve(nums, res, n, temp, st);
         return res;
     }
 };
